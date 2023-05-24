573b9a97972da270fba5c2615df8b49e6f6a1924
rnunez <rnunez@york.cuny.edu>
2023-05-24 19:22:23 -0400

    loader added not tested

--- src/View.jsx
+++ src/View.jsx
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 
 function View({data}) {
   const [element, setElement] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(data.pageURL);
-      const html = await response.text();
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, 'text/html');
-      const pageElement = doc.getElementById(data.elementID);
-      setElement(pageElement);
+      setIsLoading(true);
+      try {
+        const response = await fetch(data.pageURL);
+        const html = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const pageElement = doc.getElementById(data.elementID);
+        setElement(pageElement);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchData();
   }, [data]);
+
+  if (isLoading) {
+    return <Segment>
+      <Dimmer active inverted>
+        <Loader inverted>Loading</Loader>
+      </Dimmer>
+    </Segment>;  // Or replace with your loading indicator
+  }
 
   return element ? (
     <div dangerouslySetInnerHTML={{ __html: element.outerHTML }} />
